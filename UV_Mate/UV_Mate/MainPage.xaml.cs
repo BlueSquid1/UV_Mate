5378da90d551f72cc9b2af5ae3b42e147d131b45
blueSquid <clinton.page123@gmail.com>
2017-12-23 02:32:19 +1030

    inital commit to UV mate app

---
+++ UV_Mate/UV_Mate/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace UV_Mate
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+        private void OnPaint(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+        }
+
+        private void OnTapSample(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnPanSample(object sender, PanUpdatedEventArgs e)
+        {
+
+        }
+
+        private void OnPinchSample(object sender, PinchGestureUpdatedEventArgs e)
+        {
+
+        }
+    }
+}
